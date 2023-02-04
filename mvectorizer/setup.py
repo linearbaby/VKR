705dde4bf9c69dd2d54a471f543d904274cb531b
Artem <artemiygotovtsev@gmail.com>
2023-02-04 16:05:11 +0300

    Starting commit

---
+++ mvectorizer/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Graduation project dedicated to development of music recommendation service.',
+    author='artem',
+    license='MIT',
+)
