0d2922d0e19e2f9a8e1e5c0f62a8f449254c541d
Artem <artemiygotovtsev@gmail.com>
2023-03-30 00:01:23 +0300

    MEM now can recommend k nearest to music sample!

--- MEM/main.py
+++ MEM/main.py
@@ -2,22 +2,50 @@ from utils import model
 from fastapi import FastAPI, Header
 import numpy as np
 
+from db_connector import SimpleDFConnector
+
+
+emb_map_name = "embeddings"
+nprobe = 4
+k = 5
+
 app = FastAPI()
-eval_model = model.aquire_model()
+index = model.aquire_model(
+    "/home/artem/grad/mvectorizer/index/2023-03-19/populated.index"
+)
+index.nprobe = nprobe
+con = SimpleDFConnector(
+    data_path="/home/artem/grad/mvectorizer/data/gtzan",
+    music_location="/home/artem/grad/mvectorizer/data/gtzan/samples",
+)
+con.load_map("embeddings")
+
+
+def validate(song_id: int):
+    return True
+
 
 @app.get("/")
 def root():
     return {"Root of MEM"}
 
+
 @app.get("/query")
-def query(query_params: str | None = Header(default=None)):
-    query_params = query_params[1:-1]
-    query_params = [int(x) for x in query_params.split(",")]
-    n_query = np.atleast_2d(query_params)
+def query(song_id: int | None = Header(default=None)):
+    if validate(song_id):
+        return {
+            "eval": index.search(
+                con._get_embedding(emb_map_name, song_id)[
+                    np.newaxis,
+                ],
+                k,
+            )[1].tolist()
+        }
+    else:
+        return "error"
 
-    return {"eval": eval_model.predict(n_query).tolist()}
 
-''' 
+""" 
 query with 
-curl --header "query-params: [1, 2]" localhost:8000
-'''
+curl --header "song-id: 1" localhost:8000
+"""
--- MEM/utils/model.py
+++ MEM/utils/model.py
@@ -1,15 +1,6 @@
-from sklearn.svm import LinearSVC
-import numpy as np
+import faiss
 
-def aquire_model():
-    data = np.random.randn(100, 2)
-    data[:50] += (2, 5)
-    data[50:] += (-3, -2)
-    
-    y = np.zeros(100)
-    y[:50] = 1
 
-    model = LinearSVC()
-    model.fit(data, y)
+def aquire_model(model_path):
+    model = faiss.read_index(str(model_path))
     return model
-
