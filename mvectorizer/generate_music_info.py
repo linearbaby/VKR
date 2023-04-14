c9429ce4f95d51ec33360568b13b271ca6dab315
Artem <artemiygotovtsev@gmail.com>
2023-04-14 17:55:28 +0300

    Managed generate music for work with mysql db

--- mvectorizer/generate_music_info.py
+++ mvectorizer/generate_music_info.py
@@ -1,6 +1,10 @@
 import click
 from pathlib import Path
 import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from db_connector.src.models import Base, MusicInfo, MusicEmbedding
 
 
 @click.command()
@@ -12,15 +16,22 @@ import pandas as pd
 )
 @click.option(
     "--minfo-location",
-    required=True,
+    required=False,
     envvar="MINFO_LOCATION",
     type=click.Path(exists=True),
 )
-def construct_music_info(path_to_samples, minfo_location):
+@click.option(
+    "--type-storage",
+    required=True,
+    type=click.Choice(["DB", "Local"], case_sensitive=False),
+    default="DB",
+)
+def construct_music_info(path_to_samples, minfo_location, type_storage):
     """
     --path-to-samples - path to location of your audio files,
         relative paths to music pieces will be put in dataframe
     --minfo-location - place to store music_info
+    --type-storage - type of connector (Mysql DB or pandas DF)
     """
     genres_path = Path(path_to_samples)
     # song_id,tracks,paths
@@ -36,9 +47,38 @@ def construct_music_info(path_to_samples, minfo_location):
     paths = [paths[idx] for idx in sort_idx]
 
     # consrtuct dataframe
+    if type_storage == "DB":
+        insert_to_db(tracks, paths)
+    if type_storage == "Local":
+        construct_df(tracks, paths, Path(minfo_location) / "music_info.csv")
+
+
+def construct_df(tracks, paths, minfo_location):
+    # consrtuct dataframe
     music_info = pd.DataFrame({"tracks": tracks, "paths": paths})
     music_info.index.name = "song_id"
-    music_info.to_csv(Path(minfo_location) / "music_info.csv")
+    music_info.to_csv(minfo_location)
+
+
+def insert_to_db(tracks, paths):
+    cri = "mysql+pymysql://root:password@127.0.0.1:3306/flask"
+    engine = create_engine(cri, echo=True)
+    with Session(engine) as session:
+        Base.metadata.create_all(engine)
+        session.query(MusicEmbedding).delete()
+        session.query(MusicInfo).delete()
+
+        ids = range(1, len(tracks) + 1)
+        music_info = []
+        for (
+            id,
+            name,
+            path,
+        ) in zip(ids, tracks, paths):
+            music_info.append(MusicInfo(id=id, track_name=name, path=path))
+
+        session.add_all(music_info)
+        session.commit()
 
 
 if __name__ == "__main__":
