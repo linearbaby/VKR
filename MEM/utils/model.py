9d02b2d595ca68d56e28df6faec9de00fbdaa915
Artem <artemiygotovtsev@gmail.com>
2023-04-15 16:19:10 +0300

    Managed project to work with docker compose

--- MEM/utils/model.py
+++ MEM/utils/model.py
@@ -7,18 +7,29 @@ from .user import get_user_connector
 
 
 class PredictableTemperature:
-    def __init__(self, init_temp=5, decrease_coeff=50, end_temp=0.4):
-        self.x = decrease_coeff // init_temp
+    def __init__(self, init_temp=5, flatness_factor=4, bias=2, end_temp=0.5):
+        self.x = 0
+        self.init_temp = init_temp
         self.end_temp = end_temp
-        self.decrease_coeff = decrease_coeff
-        self.steps_before_plateu = decrease_coeff * 6
+        self.flatness_factor = flatness_factor
+        self.steps_before_plateu = flatness_factor * 6
+        self.bias = bias
+
+    def calc_temp_tanh(self):
+        return (1 - np.tanh(self.x / self.flatness_factor - self.bias)) * (
+            self.init_temp - self.end_temp
+        ) / 2 + self.end_temp
+
+    def calc_temp_hyperbol(self):
+        pass
+        # return decrease_coeff / x + end_temp
 
     def get_update_temp(self, num_recomendations):
         if self.x > self.steps_before_plateu:
             return self.end_temp
 
         self.x += num_recomendations
-        return self.decrease_coeff / self.x + self.end_temp
+        return self.calc_temp_tanh()
 
 
 class StochasticTemperature:
@@ -31,7 +42,9 @@ class RecommenderEngine:
         index_path,
         user_connector,  # dict of parameters to get_user_connector
         index_nprobe=4,
+        num_nearest=200,
         default_temperature=5,
+        distance_multiplier=15,
         temperature_descend=0.99,
         temperature_type="predictable",  # stochastic / predictable
     ):
@@ -49,9 +62,14 @@ class RecommenderEngine:
 
         self.index = faiss.read_index(str(index_path))
         self.index.nprobe = index_nprobe
-        self.num_nearest = 100
+        self.num_nearest = num_nearest
         self.default_temperature = default_temperature
         self.temperature_descend = temperature_descend
+
+        # is needed because logits from recommendation engine
+        # are located in range of [-1; 1] so, exp function doesnt work well
+        # and convenient temperature scale is not working
+        self.distance_multiplier = distance_multiplier
 
         self.user_connector = get_user_connector(**user_connector)
 
@@ -68,7 +86,7 @@ class RecommenderEngine:
         )
 
         # softmax with temperature
-        p_values = np.exp(p_values / temperature)
+        p_values = np.exp(p_values * self.distance_multiplier / temperature)
         p_values = p_values / p_values.sum()
 
         recomendations = np.random.choice(
